8d5d8067b6f27855714b3ec655b05111c83bc8de
Talon Durrant <talonmdurrant@gmail.com>
2020-04-27 17:49:23 -0600

    Updates

---
+++ precache-manifest.a548782a2f82efed156d550ee291131a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fdc4b3682a5f8a47d59115352854c1",
+    "url": "/robofriends-react-project/index.html"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/robofriends-react-project/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "d293fb30ea97274e280f",
+    "url": "/robofriends-react-project/static/css/main.742790ce.chunk.css"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/robofriends-react-project/static/js/2.2b73e027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-react-project/static/js/2.2b73e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d293fb30ea97274e280f",
+    "url": "/robofriends-react-project/static/js/main.2c0e307b.chunk.js"
+  },
+  {
+    "revision": "0e011529869c593a65f5",
+    "url": "/robofriends-react-project/static/js/runtime-main.0eacd214.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react-project/static/media/SEGA.12a3db8b.woff"
+  }
+]);
